f359dae5265da4db3518e3b9392eab771fb6ba07
ruysc <ruysilvacosta@gmail.com>
2019-11-09 12:45:26 -0300

    atualizado

--- home-home-module-es2015.js
+++ home-home-module-es2015.js
@@ -98,22 +98,22 @@ let HomePage = class HomePage {
     getMap() {
         let url = 'https://esb-hom.trt5.jus.br/justiceiros/service/healthcheck';
         this.mapUrl = 'https://maps.googleapis.com/maps/api/staticmap?center=sao%20marcos,salvador,bahia,brasil&zoom=12&size=700x700&maptype=roadmap&markers=rua%20raul%20leite%20990%20ap%20404,BA&markers=largo%202%20de%20julho,BA&markers=rio%20vermelho,BA&markers=patamares,BA&markers=paralela,BA&key=AIzaSyAwXgvxWrgfpqDSXL0_iTarEY18N09y1RM';
-        this.http.get(url, null).toPromise().then((response) => {
-            let novoResponse = response;
-            // let blob1 = new Blob([novoResponse._body], {type: 'image/png'})
-            // this.mapUrl = URL.createObjectURL(blob1)
-            //    var blob = new Blob([novoResponse._body], {
-            //   type: 'image/png'
-            // });
-            //this.mapUrl = 'data:image/png;base64,'+ novoResponse._body;
-            // this.sanitizer.bypassSecurityTrustHtml(this.mapUrl)
-            // var reader = new FileReader();
-            //     reader.readAsDataURL(blob);
-            //     reader.onloadend = () => {
-            //       this.mapUrl = reader.result;
-            //       console.log(this.mapUrl);
-            //     }
-        });
+        //this.http.get(url, null).toPromise().then((response) => {
+        //  let novoResponse: any = response;
+        // let blob1 = new Blob([novoResponse._body], {type: 'image/png'})
+        // this.mapUrl = URL.createObjectURL(blob1)
+        //    var blob = new Blob([novoResponse._body], {
+        //   type: 'image/png'
+        // });
+        //this.mapUrl = 'data:image/png;base64,'+ novoResponse._body;
+        // this.sanitizer.bypassSecurityTrustHtml(this.mapUrl)
+        // var reader = new FileReader();
+        //     reader.readAsDataURL(blob);
+        //     reader.onloadend = () => {
+        //       this.mapUrl = reader.result;
+        //       console.log(this.mapUrl);
+        //     }
+        // })
     }
     sanitize(url) {
         //return url;
